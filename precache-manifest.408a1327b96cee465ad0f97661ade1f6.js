c01f2966705d44b8e8a40a9e81a4c231b4c5a642
Harrison Bolin <harrisonbolin@gmail.com>
2020-04-14 12:31:20 -0600

    Updates

---
+++ precache-manifest.408a1327b96cee465ad0f97661ade1f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d994937203fe706b84f88dca3469a4",
+    "url": "/moviegame/index.html"
+  },
+  {
+    "revision": "46de913dee17fe134a17",
+    "url": "/moviegame/static/css/main.655a9f28.chunk.css"
+  },
+  {
+    "revision": "7484bdeec116f81ccd4e",
+    "url": "/moviegame/static/js/2.809570fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/moviegame/static/js/2.809570fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46de913dee17fe134a17",
+    "url": "/moviegame/static/js/main.e65dea93.chunk.js"
+  },
+  {
+    "revision": "5c4799f21223eecb8d9d",
+    "url": "/moviegame/static/js/runtime-main.92c5566b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/movie-game/precache-manifest.1910a8e770248225f1b8a927d66facfd.js"
+  "/moviegame/precache-manifest.408a1327b96cee465ad0f97661ade1f6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/movie-game/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/moviegame/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
